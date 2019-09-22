4f5ed81337fbcf612d9a03928cc34bdaa331b335
wwwildcat <wild--cat@yandex.ru>
2019-09-22 23:37:16 +0300

    formatting

--- pages/script.js
+++ pages/script.js
@@ -6,27 +6,27 @@ document.addEventListener('DOMContentLoaded', function() {
 		if (event.target == document.querySelector('.BranchList-arrow')) {
 			toggleBranchlist(document.body);
 		}
-	}
+	};
 });
 
 function toggleRepolist (elem) {
-    if (elem.classList && (elem.classList.contains('Icon-arrowDownBlack') || elem.classList.contains('Icon-arrowUpBlack'))) {
+	if (elem.classList && (elem.classList.contains('Icon-arrowDownBlack') || elem.classList.contains('Icon-arrowUpBlack'))) {
 		elem.classList.toggle('Icon-arrowDownBlack');
 		elem.classList.toggle('Icon-arrowUpBlack');
-    }
-    let list = document.querySelector('.RepoList');
-    if (list.classList) {
-        list.classList.toggle('RepoList_closed');
-    }
+	}
+	let list = document.querySelector('.RepoList');
+	if (list.classList) {
+		list.classList.toggle('RepoList_closed');
+	}
 }
 
 function toggleBranchlist (elem) {
-    if (elem.classList && (elem.classList.contains('Icon-arrowDownGray') || elem.classList.contains('Icon-arrowUpGray'))) {
+	if (elem.classList && (elem.classList.contains('Icon-arrowDownGray') || elem.classList.contains('Icon-arrowUpGray'))) {
 		elem.classList.toggle('Icon-arrowDownGray');
 		elem.classList.toggle('Icon-arrowUpGray');
-    }
-    let list = document.querySelector('.BranchList');
-    if (list.classList) {
-        list.classList.toggle('BranchList_closed');
-    }
+	}
+	let list = document.querySelector('.BranchList');
+	if (list.classList) {
+		list.classList.toggle('BranchList_closed');
+	}
 }
